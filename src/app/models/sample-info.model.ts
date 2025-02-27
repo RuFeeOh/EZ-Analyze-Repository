530c8d10481c95ea2319f8793ff83c5229b6cab8
Jamie G <robinson.jamie.ellen@gmail.com>
2025-02-27 17:42:11 -0500

    We made a uploadab;e table!!!

---
+++ src/app/models/sample-info.model.ts
@@ -0,0 +1,8 @@
+export class SampleInfo {
+    Location: string = "";
+    SampleNumber: number = 0;
+    SampleDate: string = "";
+    ExposureGroup: string = "";
+    Agent: string = "";
+    TWA: number = 0;
+}
--- src/app/pages/data/data.component.ts
+++ src/app/pages/data/data.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { SampleInfo } from '../../models/sample-info.model';
+import { MatTableModule } from '@angular/material/table';
 
 
 @Component({
@@ -11,14 +13,23 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    MatTableModule,
 
   ],
   templateUrl: './data.component.html',
   styleUrl: './data.component.scss'
 })
 export class DataComponent {
-  excelData: any;
-
+  excelData!: SampleInfo[];
+  columnsToDisplay = ['SampleNumber', 'SampleDate', 'ExposureGroup', 'TWA'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement!: SampleInfo | null;
+  isExpanded(element: SampleInfo) {
+    return this.expandedElement === element;
+  }
+  toggle(element: SampleInfo) {
+    this.expandedElement = this.isExpanded(element) ? null : element;
+  }
   onFileChange(event: any) {
     const file = event.target.files[0];
     const fileReader = new FileReader();
