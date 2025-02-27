bef243c7ee6814df683e8969a90d4ff4397b05c2
Jamie G <robinson.jamie.ellen@gmail.com>
2025-02-27 17:22:19 -0500

    We made a pretty button to upload a spreadsheet

--- src/app/pages/data/data.component.ts
+++ src/app/pages/data/data.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-data',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+
+  ],
   templateUrl: './data.component.html',
   styleUrl: './data.component.scss'
 })
 export class DataComponent {
+  excelData: any;
 
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      const arrayBuffer: any = fileReader.result;
+      const data = new Uint8Array(arrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      this.excelData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    };
+    fileReader.readAsArrayBuffer(file);
+  }
 }
