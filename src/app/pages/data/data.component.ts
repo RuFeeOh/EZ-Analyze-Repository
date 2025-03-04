88e255c556b124ee8b3402651d3f422fc02e2871
Jamie G <robinson.jamie.ellen@gmail.com>
2025-03-03 19:01:41 -0500

    We made an Exceedance Fraction!!! NExt step is to filter SEG

--- src/app/pages/data/data.component.ts
+++ src/app/pages/data/data.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { SampleInfo } from '../../models/sample-info.model';
 import { MatTableModule } from '@angular/material/table';
+import { ExceedanceFractionService } from '../../services/exceedance-fraction/exceedance-fraction.service';
 
 
 @Component({
@@ -20,7 +21,9 @@ import { MatTableModule } from '@angular/material/table';
   styleUrl: './data.component.scss'
 })
 export class DataComponent {
+  exceedanceFractionservice = inject(ExceedanceFractionService)
   excelData!: SampleInfo[];
+  exceedanceFraction!: number;
   columnsToDisplay = ['SampleNumber', 'SampleDate', 'ExposureGroup', 'TWA'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement!: SampleInfo | null;
@@ -41,5 +44,22 @@ export class DataComponent {
       this.excelData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
     };
     fileReader.readAsArrayBuffer(file);
+  }
+  calculateexceedanceFraction() {
+    const TWAlist: number[] = [];
+    this.excelData
+      //.filter(sample => sample.ExposureGroup === "TROUP MINING EQUIPMENT OPERATOR")
+      .forEach((sample) => {
+        if (sample.TWA && +sample.TWA > 0) {
+          TWAlist.push(+sample.TWA)
+        } else if (+sample.TWA === 0) {
+          // throw new Error('Data contains zeros')
+          alert('Data contains zeros')
+        }
+
+      })
+
+    console.log(TWAlist);
+    this.exceedanceFraction = this.exceedanceFractionservice.calculateExceedanceProbability(TWAlist, 0.01);
   }
 }
