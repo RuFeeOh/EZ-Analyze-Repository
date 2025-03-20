06daaf21f7f475309b05e9f7382fbea1dc476322
Zac Gordon <zacgord@gmail.com>
2025-03-20 12:58:50 -0400

    Refactor routing to use component references instead of lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,22 @@
 import { Routes } from '@angular/router';
+import { OrganizationSelectorComponent } from './pages/organization-selector/organization-selector.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DataComponent } from './pages/data/data.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         title: 'home',
-        loadChildren: () =>
-            import('./pages/home/home.component').then(m => m.HomeComponent)
+        component: HomeComponent,
     },
     {
         path: 'org',
         title: 'Org Selector',
-        loadChildren: () =>
-            import('./pages/organization-selector/organization-selector.component').then(m => m.OrganizationSelectorComponent)
+        component: OrganizationSelectorComponent,
     },
     {
         path: 'data',
         title: 'Data',
-        loadChildren: () =>
-            import('./pages/data/data.component').then(m => m.DataComponent)
+        component: DataComponent,
     }
 ];
