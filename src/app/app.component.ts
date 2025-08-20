9ebd0e65ccb08e54d26bbef643e86185105b786e
Zac Gordon <zacgord@gmail.com>
2025-08-20 14:11:33 -0400

    feat: refactor app layout with toolbar and enhance authentication handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,12 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { LoginComponent } from './pages/login/login.component';
 import { OrganizationService } from './services/organization/organization.service';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { Auth, GoogleAuthProvider, signInWithPopup, signOut, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -17,13 +20,34 @@ import { CommonModule } from '@angular/common';
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
-    LoginComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'EZAnalyze';
-  // inject orgazination service
+  // inject services
   public organizationService = inject(OrganizationService);
+  public auth = inject(Auth);
+  public user$ = user(this.auth);
+
+  async login() {
+    try {
+      await signInWithPopup(this.auth, new GoogleAuthProvider());
+    } catch {
+      // No-op
+    }
+  }
+
+  async logout() {
+    try {
+      this.organizationService.clearCurrentOrg();
+      await signOut(this.auth);
+    } catch {
+      // No-op
+    }
+  }
 }
